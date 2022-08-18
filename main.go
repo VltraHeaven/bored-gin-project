6e2ca3352b089e07f2a0ff3613c659cb899616bb
Julio Hawthorne <julio@vltraheaven.io>
2022-08-17 19:14:37 -0700

    Added inline get route test

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
@@ -11,5 +12,10 @@ func main() {
 			"message": "nine",
 		})
 	})
-	router.Run()
+	getUser := func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	}
+	router.GET("/user/:name", getUser)
+	router.Run("localhost:8080")
 }
