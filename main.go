9de3bf06a657b87e23698d89b035732478ba9e56
Julio Hawthorne <julio@vltraheaven.io>
2022-08-17 20:08:52 -0700

    This is an api design experiment now

--- main.go
+++ main.go
@@ -5,8 +5,58 @@ import (
 	"net/http"
 )
 
+type distro struct {
+	Name           string `json:"name"`
+	ReleaseCycle   string `json:"releaseCycle"`
+	LatestVersion  string `json:"latest"`
+	LatestKernel   string `json:"latestKernel"`
+	PackageManager string `json:"packageManager"`
+	IsImmutable    bool   `json:"isImmutable"`
+	LibC           string `json:"libc"`
+	Compiler       string `json:"compiler"`
+}
+
+var distros = []distro{
+	{
+		Name:           "ArchLinux",
+		ReleaseCycle:   "Rolling",
+		LatestVersion:  "N/A",
+		LatestKernel:   "5.19.1",
+		PackageManager: "pacman",
+		IsImmutable:    false,
+		LibC:           "glibc",
+		Compiler:       "gcc",
+	},
+	{
+		Name:           "Debian",
+		ReleaseCycle:   "Standard",
+		LatestVersion:  "11 (Bullseye)",
+		LatestKernel:   "5.10.0",
+		PackageManager: "apt/dpkg",
+		IsImmutable:    false,
+		LibC:           "glibc",
+		Compiler:       "gcc",
+	},
+	{
+		Name:           "Fedora Silverblue",
+		ReleaseCycle:   "Standard",
+		LatestVersion:  "36",
+		LatestKernel:   "5.18",
+		PackageManager: "rpm-ostree",
+		IsImmutable:    true,
+		LibC:           "glibc",
+		Compiler:       "gcc",
+	},
+}
+
+func getDistros(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, distros)
+}
+
 func main() {
 	router := gin.Default()
+	router.SetTrustedProxies([]string{"127.0.0.1", "localhost"})
+	router.GET("/distros", getDistros)
 	router.GET("/tab", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "nine",
